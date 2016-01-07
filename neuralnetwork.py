5c3c25a8c41be18fd58fe6b6454d47e14110fd88
Jake <me@jakespringer.com>
2016-01-06 22:33:56 -0800

    neuralnetwork run function implemented

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,8 +1,13 @@
 import random as rand
+import math
 
 class NeuralNetwork:
     hidden = []
     output = []
+
+    inputSize = 0
+    hiddenSize = 0
+    outputSize = 0
 
     def __init__(self, inputSize, hiddenSize, outputSize):
         self.inputSize = inputSize
@@ -10,10 +15,36 @@ class NeuralNetwork:
         self.outputSize = outputSize
         for i in range(0, hiddenSize):
             randomList = []
-            for j in range(0, inputSize):
-                randomList += [rand.random()];
+            for j in range(0, inputSize+1): # +1 for bias
+                randomList += [rand.random()]
             self.hidden += [randomList]
 
+        for i in range(0, outputSize):
+            randomList = []
+            for j in range(0, hiddenSize+1): # +1 for bias
+                randomList += [rand.random()]
+            self.output += [randomList]
 
-neural = NeuralNetwork(12, 3, 4)
+    def train(self, input, outputTarget):
+        self
+
+    def run(self, input):
+        result = []
+        inputWithBias = input + [1]
+        for o in range(0, self.outputSize):
+            hiddenSum = 0
+            for h in range(0, self.hiddenSize):
+                inputSum = 0
+                for i in range(0, self.inputSize+1): # bias
+                    inputSum += inputWithBias[i] * self.hidden[h][i]
+                hiddenSum += activate(inputSum) * self.output[o][h]
+            hiddenSum += self.output[o][self.hiddenSize] # bias
+            result += [activate(hiddenSum)]
+        return result
+
+def activate(x):
+    return 1.0/(1.0+math.exp(-x))
+
+neural = NeuralNetwork(2, 3, 4)
+print(neural.run([0.5, 0.2]))
 print(neural.hidden)
